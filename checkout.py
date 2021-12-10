d2e7eade593a6510481846ec2fbce9992816b8a1
janw <janwolfram@gmx.net>
2021-12-10 20:25:52 +0100

    newest version of checkout.py

--- checkout.py
+++ checkout.py
@@ -11,17 +11,41 @@ def setupArgparse(branches):
         shortform = ""
         for word in splitted:
             shortform += word[0]
-        group.add_argument("+{}".format(shortform), "++{}".format(branch), help="checkout {} in src".format(branch), action="store_true")    
+        group.add_argument("+{}".format(shortform), "++{}".format(branch), help="checkout {} in src".format(branch), action="store_const", const=1, default=0)    
+        group.add_argument("-{}".format(shortform), "--{}".format(branch), help="remove {} in src".format(branch), action="store_const", const=2, default=0)
     return parser.parse_args()
+
+def handleFlags(flags):
+    print(flags)
+    for branch_name in flags:
+        # checkout branches
+        if flags[branch_name] == 1:
+            if "src" in os.path.dirname(os.getcwd()):
+                cmd = "git worktree add ../{}".format(branch_name)
+                os.system(cmd)
+            else:    
+                cmd = "git worktree add src/{}".format(branch_name)
+                os.system(cmd)
+        # remove branches
+        if flags[branch_name] == 2: 
+            if "src" in os.path.dirname(os.getcwd()):
+                cmd = "git worktree remove ../{}".format(branch_name)
+                os.system(cmd)
+            else:    
+                cmd = "git worktree remove src/{}".format(branch_name)   
+                os.system(cmd)
+            
+
 
 def main():
     branches = open("branches","r").read().splitlines()
     args = setupArgparse(branches)
-    test = vars(args)
-    for branch_name in test:
-        if test[branch_name]:
-            cmd = "git worktree add src/{}".format(branch_name)
-            os.system(cmd)
+    print(args)
+    # arguments to dict
+    flags = vars(args)
+    handleFlags(flags)
+
+    
 
 if __name__ == '__main__':
     main()
