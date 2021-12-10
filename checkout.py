c2bd6b3c3a7017bd734ac424c05f889ffde492d7
janw <janwolfram@gmx.net>
2021-12-10 15:10:58 +0100

    better script

--- checkout.py
+++ checkout.py
@@ -1,23 +1,27 @@
-import argparse
+#!/usr/bin/python3
+import argparse, os
 
-def setupArgparse():
-    parser = argparse.ArgumentParser(description="Checkout your project branches")
-    parser.add_argument("-d", "--deploy", help="additional develop branch", action="store_true")
+def setupArgparse(branches):
+    parser = argparse.ArgumentParser(description="Checkout your project branches", prefix_chars='-+')
+    # parser.add_argument("-d", "--deploy", help="additional develop branch", action="store_true")
 
     group = parser.add_mutually_exclusive_group()
-    # group.add_argument("-d", "--dev", help="additional develop branch", action="store_true")
-    group.add_argument("-m", "--main", help="checkout main project", action="store_true")
-    group.add_argument('-b', '--bootloader', help="checkout bootloader project", action="store_true")
-
+    for branch in branches:
+        splitted = branch.split("_")
+        shortform = ""
+        for word in splitted:
+            shortform += word[0]
+        group.add_argument("+{}".format(shortform), "++{}".format(branch), help="checkout {} in src".format(branch), action="store_true")    
     return parser.parse_args()
 
-
 def main():
-    args = setupArgparse()
-    if args.main:
-        command = "mkdir src && git worktree add src/webportal && git worktree add src/tx_firmware && git worktree add src/tx_app_server"
-        print(args.main)
-
+    branches = open("branches","r").read().splitlines()
+    args = setupArgparse(branches)
+    test = vars(args)
+    for branch_name in test:
+        if test[branch_name]:
+            cmd = "git worktree add src/{}".format(branch_name)
+            os.system(cmd)
 
 if __name__ == '__main__':
     main()
