6c7f6d5eecea8681490e17ff5fa780da43ed7fab
janw <janwolfram@gmx.net>
2021-12-11 11:51:52 +0100

    next version

--- checkout.py
+++ checkout.py
@@ -3,22 +3,24 @@ import argparse, os
 
 def setupArgparse(branches):
     parser = argparse.ArgumentParser(description="Checkout your project branches", prefix_chars='-+')
-    # parser.add_argument("-d", "--deploy", help="additional develop branch", action="store_true")
 
+    # automatically create flags from branches.txt
     group = parser.add_mutually_exclusive_group()
     for branch in branches:
         splitted = branch.split("_")
         shortform = ""
         for word in splitted:
             shortform += word[0]
+        # flag to checkout a branch + deploy branch
         group.add_argument("+{}".format(shortform), "++{}".format(branch), help="checkout {} in src".format(branch), action="store_const", const=1, default=0)    
+        # flag to remove a branch + deploy branch
         group.add_argument("-{}".format(shortform), "--{}".format(branch), help="remove {} in src".format(branch), action="store_const", const=2, default=0)
     return parser.parse_args()
 
 
 def createDeployBranch(branch_name, src_path):
     # create local deploy branch
-    cmd = "cd {}{} ; git checkout -b {}_deploy ; git checkout {} ; cd .. ; cd ..; git worktree add deploy/{}_deploy".format(src_path, branch_name, branch_name, branch_name)
+    cmd = "cd {}{} ; git checkout -b {}_deploy ; git checkout {} ; cd .. ; cd ..; git worktree add deploy/{}_deploy".format(src_path, branch_name, branch_name, branch_name, branch_name)
     os.system(cmd)
 
 
@@ -41,17 +43,10 @@ def handleFlags(flags):
 
         # remove branches
         if flags[branch_name] == 2: 
-            if "src" in os.path.dirname(os.getcwd()) or "deploy" in os.path.dirname(os.getcwd()):
-                cmd = "git worktree remove ../{}".format(branch_name)
-                os.system(cmd)
-                cmd = "cd ..; cd .. ; cd deploy; rm -r {}; cd ..; git worktree prune".format(branch_name)
+            cmd = "git worktree remove {} ; git worktree remove {}_deploy".format(branch_name, branch_name)
+            os.system(cmd)
 
-            else:    
-                cmd = "git worktree remove src/{}".format(branch_name)   
-                os.system(cmd)
-                cmd = "cd deploy; rm -r {}; cd ..; git worktree prune".format(branch_name)
-
-
+           
 def main():
     branches = open("branches","r").read().splitlines()
     args = setupArgparse(branches)
