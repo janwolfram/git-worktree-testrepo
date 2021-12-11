9a2231f6e40a458b8401ffc7d763048559aecdd5
janw <janwolfram@gmx.net>
2021-12-11 11:38:47 +0100

    finish

--- checkout.py
+++ checkout.py
@@ -15,26 +15,41 @@ def setupArgparse(branches):
         group.add_argument("-{}".format(shortform), "--{}".format(branch), help="remove {} in src".format(branch), action="store_const", const=2, default=0)
     return parser.parse_args()
 
+
+def createDeployBranch(branch_name, src_path):
+    # create local deploy branch
+    cmd = "cd {}{} ; git checkout -b {}_deploy ; git checkout {} ; cd .. ; cd ..; git worktree add deploy/{}_deploy".format(src_path, branch_name, branch_name, branch_name)
+    os.system(cmd)
+
+
 def handleFlags(flags):
     print(flags)
     for branch_name in flags:
         # checkout branches
         if flags[branch_name] == 1:
-            if "src" in os.path.dirname(os.getcwd()):
-                cmd = "git worktree add ../{}".format(branch_name)
+            if "src" in os.path.dirname(os.getcwd()) or "deploy" in os.path.dirname(os.getcwd()):
+                cmd = "cd ..; cd ..; git worktree add src/{}".format(branch_name)
                 os.system(cmd)
+                # create local deploy branch
+                createDeployBranch(branch_name, "../../src")
+                
             else:    
                 cmd = "git worktree add src/{}".format(branch_name)
                 os.system(cmd)
+                # create local deploy branch
+                createDeployBranch(branch_name, "src/")
+
         # remove branches
         if flags[branch_name] == 2: 
-            if "src" in os.path.dirname(os.getcwd()):
+            if "src" in os.path.dirname(os.getcwd()) or "deploy" in os.path.dirname(os.getcwd()):
                 cmd = "git worktree remove ../{}".format(branch_name)
                 os.system(cmd)
+                cmd = "cd ..; cd .. ; cd deploy; rm -r {}; cd ..; git worktree prune".format(branch_name)
+
             else:    
                 cmd = "git worktree remove src/{}".format(branch_name)   
                 os.system(cmd)
-            
+                cmd = "cd deploy; rm -r {}; cd ..; git worktree prune".format(branch_name)
 
 
 def main():
@@ -46,6 +61,5 @@ def main():
     handleFlags(flags)
 
     
-
 if __name__ == '__main__':
     main()
